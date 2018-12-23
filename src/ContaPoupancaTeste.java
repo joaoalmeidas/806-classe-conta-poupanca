ce7ee3a1499e0801980d619d064baec42a037e39
joao <joao182cx@gmail.com>
2018-12-23 14:42:43 -0200

    Adição de método para exibir apenas um mês do saldo da conta.

--- src/ContaPoupancaTeste.java
+++ src/ContaPoupancaTeste.java
@@ -8,14 +8,18 @@ public class ContaPoupancaTeste {
 		
 		ContaPoupanca.modificaTaxaDeJuros(4);
 		
-		exibeJurosClientes(cliente1, cliente2);
+		exibeJurosClientesUmAno(cliente1, cliente2);
 		
 		ContaPoupanca.modificaTaxaDeJuros(5);
 		
-		exibeJurosClientes(cliente1, cliente2);
+		cliente1.calculaJurosMensal();
+		cliente2.calculaJurosMensal();
+		
+		exibeJurosClientesUmMes(cliente1, cliente2);
+		
 	}
 	
-	public static void exibeJurosClientes(ContaPoupanca... conta) {
+	public static void exibeJurosClientesUmAno(ContaPoupanca... conta) {
 		
 		System.out.println("Saldo dos clientes durante doze meses com uma taxa de juros de " +ContaPoupanca.getTaxaDeJurosAnual()+ " ao ano\n");
 		
@@ -28,6 +32,35 @@ public class ContaPoupancaTeste {
 		System.out.println();
 		
 		for(int i = 1; i <= 12; i++) {
+			
+			for(int j = 0; j < conta.length; j++) {
+				
+				System.out.printf("%.2f\t\t", conta[j].getSaldoPoupanca());
+				conta[j].calculaJurosMensal();
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+		System.out.println("\n");
+		
+	}
+	
+	public static void exibeJurosClientesUmMes(ContaPoupanca... conta) {
+		
+		System.out.println("Saldo dos clientes durante doze meses com uma taxa de juros de " +ContaPoupanca.getTaxaDeJurosAnual()+ " ao ano\n");
+		
+		for(int i = 0; i < conta.length; i++) {
+			
+			System.out.printf("Cliente %d\t", i+1);	
+			
+		}
+		
+		System.out.println();
+		
+		for(int i = 1; i <= 1; i++) {
 			
 			for(int j = 0; j < conta.length; j++) {
 				
