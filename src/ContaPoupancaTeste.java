3fc93cabfc10564203db101b83412584c9f761d7
joao <joao182cx@gmail.com>
2018-12-23 14:06:46 -0200

    Criação da classe de teste.

--- src/ContaPoupancaTeste.java
+++ src/ContaPoupancaTeste.java
@@ -2,8 +2,37 @@
 public class ContaPoupancaTeste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ContaPoupanca cliente1 = new ContaPoupanca(2000);
+		ContaPoupanca cliente2 = new ContaPoupanca(3000);
+		ContaPoupanca.modificaTaxaDeJuros(4);
+		
+		exibeJurosClientes(cliente1, cliente2);
 
 	}
-
+	
+	public static void exibeJurosClientes(ContaPoupanca... conta) {
+		
+		for(int i = 0; i < conta.length; i++) {
+			
+			System.out.printf("Cliente %d\t", i);	
+			
+		}
+		
+		System.out.println();
+		
+		for(int i = 1; i <= 12; i++) {
+			
+			for(int j = 0; j < conta.length; j++) {
+				
+				System.out.printf("%.2f\t", conta[j].getSaldoPoupanca());
+				conta[j].calculaJurosMensal();
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+	}
 }
