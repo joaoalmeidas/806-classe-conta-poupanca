3b008a2d9859fc3e7e816c3defa3176849305db3
joao <joao182cx@gmail.com>
2018-12-23 13:44:23 -0200

    Criação dos atributos da classe ContaPoupanca e os seu métodos get e set.

--- src/ContaPoupanca.java
+++ src/ContaPoupanca.java
@@ -1,4 +1,35 @@
 
 public class ContaPoupanca {
+	
+	private double saldoPoupanca;
+	
+	static double taxaDeJurosAnual;
+	
+	
+	public void calculaJurosMensal() {
+		setSaldoPoupanca(getSaldoPoupanca() + (getSaldoPoupanca() * getTaxaDeJurosAnual()/12));
+	}
 
+
+	public double getSaldoPoupanca() {
+		return saldoPoupanca;
+	}
+
+
+	public void setSaldoPoupanca(double saldoPoupanca) {
+		this.saldoPoupanca = saldoPoupanca;
+	}
+
+
+	public static double getTaxaDeJurosAnual() {
+		return taxaDeJurosAnual;
+	}
+
+
+	public static void setTaxaDeJurosAnual(double taxaDeJurosAnual) {
+		ContaPoupanca.taxaDeJurosAnual = taxaDeJurosAnual;
+	}
+	
+	
+	
 }
